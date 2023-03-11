286cb45decbbed464a3ebc95430e0d5913639e6e
Pancratzia <ortegalaura180999@gmail.com>
2023-03-11 10:09:33 -0400

    Se termina el proyecto: checkboxes

--- 32-Checkboxes/main.js
+++ 32-Checkboxes/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const toggles = document.querySelectorAll('.toggle');
+const bueno = document.getElementById('bueno');
+const bonito = document.getElementById('bonito');
+const barato = document.getElementById('barato');
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('change', (e) => truco(e.target));
+})
+
+function truco(seleccionado) {
+   if(bueno.checked && bonito.checked && barato.checked){
+       if(bueno===seleccionado){
+        barato.checked = false;
+       }
+
+       if(bonito===seleccionado){
+        bueno.checked = false;
+       }
+
+       if(barato===seleccionado){
+        bonito.checked = false;
+       }
+   }
+}
